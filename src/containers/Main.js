301400f6dbab4fc384c20a5920e768f3fc6f26b6
Caio Monteiro <caiomonteirooeng@gmail.com>
2025-08-12 22:19:42 -0300

    homepage nao estava carregando no github pages

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -25,7 +25,10 @@ export default class Main extends Component {
               )
             }
           />
-
+          <Route
+            path="/Portfolio"
+            render={(props) => <Home {...props} theme={this.props.theme} />}
+          />
           <Route
             path="/inicio"
             render={(props) => <Home {...props} theme={this.props.theme} />}
